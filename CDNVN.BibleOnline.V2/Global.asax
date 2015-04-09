57c498f242d850ec6ca2b4d609fc4505fbe853c4
kenboy2304 <kenboy2304@gmail.com>
2015-04-09 15:01:06 +0700

    The First

---
+++ CDNVN.BibleOnline.V2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CDNVN.BibleOnline.V2.MvcApplication" Language="C#" %>
---
+++ CDNVN.BibleOnline.V2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CDNVN.BibleOnline.V2.Startup))]
+namespace CDNVN.BibleOnline.V2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
